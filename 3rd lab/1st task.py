da5b16de85aba627c558565c049e68142ce081de
JuSsimb0 <147836387+JuSsimb0@users.noreply.github.com>
2023-10-29 02:46:09 +0300

    Лабораторная работа 3
    Лабораторная работа "Функции. Создание и использование функций"

---
+++ 3rd lab/1st task.py
@@ -0,0 +1,16 @@
+def search_in_list(total_list, item):
+    for num, current_item in enumerate(total_list):
+        if current_item == item:
+            index_of_item = num
+            return index_of_item
+    return None
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = search_in_list(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
